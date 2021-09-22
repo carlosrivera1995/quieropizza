0164d901e358b994fcf2443aef749c37c6bfea97
proyecto <carlosr191995@gmail.com>
2021-09-21 18:54:51 -0600

    Agregando arquitectura basica

--- Ferreteria/Ferreteria/Ferreteria.Web/Controllers/ProductosController.cs
+++ Ferreteria/Ferreteria/Ferreteria.Web/Controllers/ProductosController.cs
@@ -1,4 +1,4 @@
-﻿using Ferreteria.Web.Models;
+﻿using Ferreteria.BL;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,23 +12,9 @@ namespace Ferreteria.Web.Controllers
         // GET: Productos
         public ActionResult Index()
         {
-            var producto1 = new ProductoModel();
-            producto1.Id = 1;
-            producto1.Descripcion = "Llave de Perica";
 
-            var producto2 = new ProductoModel();
-            producto2.Id = 2;
-            producto2.Descripcion = "Caja Herramientas";
-
-            var producto3 = new ProductoModel();
-            producto3.Id = 3;
-            producto3.Descripcion = "Varilla de Hierro";
-
-            var listadeProductos = new List<ProductoModel>();
-            listadeProductos.Add(producto1);
-            listadeProductos.Add(producto2);
-            listadeProductos.Add(producto3);
-
+            var productosBL = new ProductosBL();
+            var listadeProductos = productosBL.ObtenerProductos();
 
             return View(listadeProductos);
         }
