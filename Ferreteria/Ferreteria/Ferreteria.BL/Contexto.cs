40490dd566449a389e14209c2d7b62fdab164f3d
proyecto <carlosr191995@gmail.com>
2021-09-22 22:41:48 -0600

    Agregando cambios de I parte

---
+++ Ferreteria/Ferreteria/Ferreteria.BL/Contexto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ferreteria.BL
+{
+    public class Contexto : DbContext
+    {
+        public Contexto() : base(@"Data Source=(LocalDb)\MSSQLLocalDB;AttachDBFilename=" +
+            Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\FerreteriaDB.mdf")
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+
+        public DbSet<Producto> Productos { get; set; }
+    }
+}
--- Ferreteria/Ferreteria/Ferreteria.BL/ProductosBL.cs
+++ Ferreteria/Ferreteria/Ferreteria.BL/ProductosBL.cs
@@ -8,29 +8,19 @@ namespace Ferreteria.BL
 {
     public class ProductosBL
     {
+        Contexto _contexto;
+        public List<Producto> ListadeProductos { get; set; }
+
+        public ProductosBL()
+        {
+          _contexto = new Contexto();
+            ListadeProductos = new List<Producto>();
+        }
+
         public List<Producto> ObtenerProductos()
         {
-            var producto1 = new Producto();
-            producto1.Id = 1;
-            producto1.Descripcion = "Llave de Perica";
-            producto1.Precio = 100;
-
-            var producto2 = new Producto();
-            producto2.Id = 2;
-            producto2.Descripcion = "Caja Herramientas";
-            producto2.Precio = 200;
-
-            var producto3 = new Producto();
-            producto3.Id = 3;
-            producto3.Descripcion = "Varilla de Hierro";
-            producto3.Precio = 20;
-
-            var listadeProductos = new List<Producto>();
-            listadeProductos.Add(producto1);
-            listadeProductos.Add(producto2);
-            listadeProductos.Add(producto3);
-
-            return listadeProductos;
+            ListadeProductos = _contexto.Productos.ToList();
+            return _contexto.Productos.ToList();
         }
     }
 }
